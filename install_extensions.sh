5255229c5daaa4d88e448bfc3f315902dc0cd07d
Gatz FO <gsandrof66@gmail.com>
2026-05-05 10:33:19 +0000

    updated extensions 1

--- install_extensions.sh
+++ install_extensions.sh
@@ -21,35 +21,35 @@ unset NODE_EXTRA_CA_CERTS
 
 # base extensions
 BASE_URL=(
-  "https://open-vsx.org/api/ms-python/python/2025.16.0/file/ms-python.python-2025.16.0.vsix"
-  "https://open-vsx.org/api/ms-python/debugpy/linux-x64/2025.14.1/file/ms-python.debugpy-2025.14.1@linux-x64.vsix"
+  "https://open-vsx.org/api/ms-python/python/2026.4.0/file/ms-python.python-2026.4.0.vsix"
+  "https://open-vsx.org/api/ms-python/debugpy/linux-x64/2026.6.0/file/ms-python.debugpy-2026.6.0@linux-x64.vsix"
   "https://open-vsx.org/api/njpwerner/autodocstring/0.6.1/file/njpwerner.autodocstring-0.6.1.vsix"
-  "https://open-vsx.org/api/ms-toolsai/jupyter/2025.7.0/file/ms-toolsai.jupyter-2025.7.0.vsix"
-  "https://open-vsx.org/api/ms-toolsai/jupyter-renderers/1.0.19/file/ms-toolsai.jupyter-renderers-1.0.19.vsix"
+  "https://open-vsx.org/api/ms-toolsai/jupyter/2025.9.1/file/ms-toolsai.jupyter-2025.9.1.vsix"
+  "https://open-vsx.org/api/ms-toolsai/jupyter-renderers/1.3.0/file/ms-toolsai.jupyter-renderers-1.3.0.vsix"
   "https://open-vsx.org/api/ms-toolsai/vscode-jupyter-slideshow/0.1.6/file/ms-toolsai.vscode-jupyter-slideshow-0.1.6.vsix"
   "https://open-vsx.org/api/ms-toolsai/vscode-jupyter-cell-tags/0.1.9/file/ms-toolsai.vscode-jupyter-cell-tags-0.1.9.vsix"
   "https://open-vsx.org/api/ms-toolsai/jupyter-keymap/1.1.2/file/ms-toolsai.jupyter-keymap-1.1.2.vsix"
-  "https://open-vsx.org/api/vscode-icons-team/vscode-icons/12.15.0/file/vscode-icons-team.vscode-icons-12.15.0.vsix"
-  "https://open-vsx.org/api/esbenp/prettier-vscode/11.0.0/file/esbenp.prettier-vscode-11.0.0.vsix"
-  "https://open-vsx.org/api/mechatroner/rainbow-csv/3.3.0/file/mechatroner.rainbow-csv-3.3.0.vsix"
-  "https://open-vsx.org/api/eamodio/gitlens/2026.3.423/file/eamodio.gitlens-2026.3.423.vsix"
-  "https://open-vsx.org/api/posit/air-vscode/linux-x64/0.16.0/file/posit.air-vscode-0.16.0@linux-x64.vsix"
-  "https://open-vsx.org/api/posit/shiny/1.3.1/file/posit.shiny-1.3.1.vsix"
-  "https://open-vsx.org/api/quarto/quarto/1.126.0/file/quarto.quarto-1.126.0.vsix"
+  "https://open-vsx.org/api/vscode-icons-team/vscode-icons/12.18.0/file/vscode-icons-team.vscode-icons-12.18.0.vsix"
+  "https://open-vsx.org/api/esbenp/prettier-vscode/12.2.0/file/esbenp.prettier-vscode-12.2.0.vsix"
+  "https://open-vsx.org/api/eamodio/gitlens/2026.5.40551/file/eamodio.gitlens-2026.5.40551.vsix"
+  "https://open-vsx.org/api/posit/air-vscode/linux-x64/0.24.0/file/posit.air-vscode-0.24.0@linux-x64.vsix"
+  "https://open-vsx.org/api/posit/shiny/1.4.1/file/posit.shiny-1.4.1.vsix"
+  "https://open-vsx.org/api/quarto/quarto/1.131.0/file/quarto.quarto-1.131.0.vsix"
   "https://open-vsx.org/api/ms-pyright/pyright/1.1.402/file/ms-pyright.pyright-1.1.402.vsix"
-  "https://open-vsx.org/api/charliermarsh/ruff/linux-x64/2025.28.0/file/charliermarsh.ruff-2025.28.0@linux-x64.vsix"
+  "https://open-vsx.org/api/meta/pyrefly/linux-x64/0.63.1/file/meta.pyrefly-0.63.1@linux-x64.vsix"
+  "https://open-vsx.org/api/charliermarsh/ruff/linux-x64/2026.40.0/file/charliermarsh.ruff-2026.40.0@linux-x64.vsix"
   "https://open-vsx.org/api/posit/shinyuieditor/0.5.0/file/posit.shinyuieditor-0.5.0.vsix"
   "https://open-vsx.org/api/hediet/vscode-drawio/1.6.6/file/hediet.vscode-drawio-1.6.6.vsix"
 )
 
 # add java extensions
 JAVA_URL=(
-  "https://open-vsx.org/api/vscjava/vscode-java-dependency/0.26.3/file/vscjava.vscode-java-dependency-0.26.3.vsix"
-  "https://open-vsx.org/api/vscjava/vscode-maven/0.44.0/file/vscjava.vscode-maven-0.44.0.vsix"
-  "https://open-vsx.org/api/vscjava/vscode-gradle/3.17.1/file/vscjava.vscode-gradle-3.17.1.vsix"
-  "https://open-vsx.org/api/vscjava/vscode-java-test/0.43.2/file/vscjava.vscode-java-test-0.43.2.vsix"
-  "https://open-vsx.org/api/redhat/java/linux-x64/1.51.2025121108/file/redhat.java-1.51.2025121108@linux-x64.vsix"
-  "https://open-vsx.org/api/vscjava/vscode-java-debug/0.58.3/file/vscjava.vscode-java-debug-0.58.3.vsix"
+  "https://open-vsx.org/api/vscjava/vscode-java-dependency/0.27.2/file/vscjava.vscode-java-dependency-0.26.3.vsix"
+  "https://open-vsx.org/api/vscjava/vscode-maven/0.45.3/file/vscjava.vscode-maven-0.44.0.vsix"
+  "https://open-vsx.org/api/vscjava/vscode-gradle/3.17.3/file/vscjava.vscode-gradle-3.17.1.vsix"
+  "https://open-vsx.org/api/vscjava/vscode-java-test/0.45.0/file/vscjava.vscode-java-test-0.43.2.vsix"
+  "https://open-vsx.org/api/redhat/java/linux-x64/1.55.2026050508/file/redhat.java-1.55.2026050508@linux-x64.vsix"
+  "https://open-vsx.org/api/vscjava/vscode-java-debug/0.59.0/file/vscjava.vscode-java-debug-0.58.3.vsix"
 )
 
 # Check the value of ide
@@ -60,6 +60,7 @@ if [ "$ide" == "vscode" ]; then
     
     VS_URL=(
       "https://open-vsx.org/api/formulahendry/code-runner/0.12.2/file/formulahendry.code-runner-0.12.2.vsix"
+      "https://open-vsx.org/api/mechatroner/rainbow-csv/3.24.1/file/mechatroner.rainbow-csv-3.24.1.vsix"
     )
 
     BASE_URL=( "${BASE_URL[@]}" "${VS_URL[@]}" )
@@ -69,7 +70,8 @@ elif [ "$ide" == "positron" ]; then
     echo "You selected Positron"
 
     POSIT_URL=(
-      "https://open-vsx.org/api/posit/publisher/linux-x64/1.27.10/file/posit.publisher-1.27.10@linux-x64.vsix"
+      "https://open-vsx.org/api/posit/publisher/linux-x64/1.36.0/file/posit.publisher-1.27.10@linux-x64.vsix"
+      "https://open-vsx.org/api/posit/publisher/2.5.1/file/posit.publisher-2.5.1.vsix"
     )
 
     BASE_URL=( "${BASE_URL[@]}" "${POSIT_URL[@]}" )
